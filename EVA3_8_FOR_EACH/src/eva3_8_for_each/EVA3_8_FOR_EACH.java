e445b15de9d744b1ea7b661e33d61b876ed3cfb9
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-12-01 23:42:24 -0600

    EVA3_8_FOR_EACH

---
+++ EVA3_8_FOR_EACH/src/eva3_8_for_each/EVA3_8_FOR_EACH.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_8_for_each;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA3_8_FOR_EACH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String[] dias {"lunes","martes","miercoles","jueves ","viernes","sabado","domingo"};
+        /*for (int i = 0; i<dias.length;i ++);{
+        System.out.println(dias.[i]);*/
+        
+        for (string cade = dias);{
+        System.out.println(cade); 
+    }
+        int[]billetes =(20,50,100,200,500,1000);
+        for billetes
+    }
+    
+}
