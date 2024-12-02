a38d111a29fc2eb453a63889f25ffaec4950eb61
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-12-01 23:39:41 -0600

    EVA3_1_METODOS

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     imprimirMensaje("HOLA MUNDO");  
+    }
+    public static void imprimirMensaje(String mensaje,String otro){
+            System.out.println("Tu Mensaje:"+ mensaje);
+                 }
+    
+}
